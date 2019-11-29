22f49efdaf21c7629f9f43a086cd357ff71d2c35
Stefan Basaric <st111465@stud.uni-stuttgart.de>
2019-11-29 08:50:57 +0100

    Init commit

---
+++ src/main/java/de/loosecoupling/assignment3/stockagent/StockAgentApplication.java
@@ -0,0 +1,13 @@
+package de.loosecoupling.assignment3.stockagent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockAgentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StockAgentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/loosecoupling/assignment3/stockagent/StockAgentApplicationTests.java
@@ -0,0 +1,13 @@
+package de.loosecoupling.assignment3.stockagent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockAgentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
